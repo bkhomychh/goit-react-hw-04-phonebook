131919b3cbf204a8a28f645ca1f773ac7db8c210
Bohdan Khomych <bod.khomych@gmail.com>
2023-03-03 11:59:14 +0200

    save contacts to local storage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,19 +4,26 @@ import { nanoid } from 'nanoid';
 import { ContactForm } from 'components/ContactForm';
 import { Filter } from 'components/Filter';
 import { ContactList } from 'components/ContactList';
+import { saveDataToStorage, getDataFromStorage } from 'helpers/storage';
 
 import { Container, Title, Heading } from './App.styled';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const data = getDataFromStorage('contacts');
+    const contacts = data ? data : [];
+
+    this.setState({ contacts });
+  }
+
+  componentDidUpdate() {
+    saveDataToStorage('contacts', this.state.contacts);
+  }
 
   addContact = ({ name, number }) => {
     const isNew = !this.checkContactExistence(name);
